ebb284a6f1fc244b697d58896f8bef9593aea43a
Divyanshi Sharma <146567651+divs16@users.noreply.github.com>
2025-04-30 11:53:47 -0700

    Update and rename video_API_calls.txt to video_API_calls.py
    commented out output to keep it in the same file

--- Vandalism/video_API_calls.py
+++ Vandalism/video_API_calls.py
@@ -41,7 +41,9 @@ try:
 except Exception as e:
     print(f" Gemini API error: {e}\n")
 ​
+'''
 /home/realtimeidns/genai-vandalism-env/lib/python3.12/site-packages/tqdm/auto.py:21: TqdmWarning: IProgress not found. Please update jupyter and ipywidgets. See https://ipywidgets.readthedocs.io/en/stable/user_install.html
   from .autonotebook import tqdm as notebook_tqdm
 🤖 Gemini Response:
-Yes: breaking
+Yes: breaking life
+'''
