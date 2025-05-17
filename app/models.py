920adab187794e4e856bfb3391b058068a2340aa
Manav Rajiv Moorthy <manavrm@uw.edu>
2025-05-17 15:33:34 -0700

    edits to models.py

--- app/models.py
+++ app/models.py
@@ -1,24 +1,6 @@
-from sqlalchemy import Column, String, Float, Boolean, DateTime, Text
-from database import Base
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
-
-
-class VideoMetadata(Base):
-    __tablename__ = "video_metadata"
-    __table_args__ = {"schema": "ga8ed"}
-
-    video_id = Column(String, primary_key=True)
-    video_uri = Column(String)
-    tracked_predictions_uri = Column(String)
-    annotated_video_uri = Column(String, nullable=True)
-    stability_score = Column(Float, nullable=True)
-    bear_alert = Column(Boolean, nullable=True)
-    vandalism_genai_response = Column(Text, nullable=True)
-    vandalism_alert = Column(Boolean, nullable=True)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
 
 
 class VideoResponse(BaseModel):
